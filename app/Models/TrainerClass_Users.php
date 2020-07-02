392b28aa02dbea48ae4eddd27477e304fe127004
AbubakarMian <AbubakarMian@users.noreply.github.com>
2020-07-02 21:41:39 +0500

    models and migrations

--- app/Models/TrainerClass_Users.php
+++ app/Models/TrainerClass_Users.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class TrainerClass_Users extends Model
 {
-    //
+    
+        use SoftDeletes;
+    protected $table='trainerclass_users';
 }
