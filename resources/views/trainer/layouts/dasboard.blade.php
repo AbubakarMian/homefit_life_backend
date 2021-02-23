1447c63dfa77fac7c2f2da27dec636f805b34e68
arhambutt11 <72912177+arhambutt11@users.noreply.github.com>
2021-02-23 15:18:36 -0800

    CSS FIXES

--- resources/views/trainer/layouts/dasboard.blade.php
+++ resources/views/trainer/layouts/dasboard.blade.php
@@ -7,105 +7,42 @@
             <span>Trainer</span> DASHBOARD
         </div>
         <ul class="nav">
-            <!-- dashboard -->
             <li>
-                <a href="http://localhost/quran_19_lines_backend/public/admin/dashboard">
-                    <i class="ti-home"></i>
-                    <span>Dashboard</span>
+                <a href="#">
+                    <span><i class="fa fa-tachometer dashicon" aria-hidden="true"></i>Dashboard</span>
                 </a>
             </li>
-            <!-- /dashboard -->
             <li>
-                <a href="javascript:;">
-                    <i class="toggle-accordion"></i>
-                    <!-- <i class="ti-support"></i> -->
-                   
-                    <span>Group Classes</span>
+                <a href="#">
+                    <span><i class="fa fa-user dashicon" aria-hidden="true"></i>My Profile</span>                    
                 </a>
-                <ul class="sub-menu" style="display: none;">
-                <a href="javascript:;">
-                    <i class="toggle-accordion"></i>
-                    <!-- <i class="ti-support"></i> -->
-                    <i class="fa fa-line-chart"></i>
-                    <span>- Host a Group Classes</span>
-                </a>
-                <a href="javascript:;">
-                    <i class="toggle-accordion"></i>
-                    <!-- <i class="ti-support"></i> -->
-                    <i class="fa fa-line-chart"></i>
-                    <span>- Group Classes</span>
-                </a>
-               
-                </ul>
-            </li>  
-          
+            </li>
+
             <li>
-                <a href="javascript:;">
-                    <i class="toggle-accordion"></i>
-                    <!-- <i class="ti-support"></i> -->
-                    <i class="fa fa-line-chart"></i>
-                    <span>Live Sessions</span>
-                </a>
-                <ul class="sub-menu">
-                <a href="javascript:;">
-                    <i class="toggle-accordion"></i>
-                    <!-- <i class="ti-support"></i> -->
-                    <i class="fa fa-line-chart"></i>
-                    <span>- Paid Sessions</span>
-                </a>
-                <a href="javascript:;">
-                    <i class="toggle-accordion"></i>
-                    <!-- <i class="ti-support"></i> -->
-                    <i class="fa fa-line-chart"></i>
-                    <span>- Unpaid Sessions</span>
-                </a>
-               
+                <a href="#" data-toggle="collapse" data-target="#submenu-1" class="collapsed" aria-expanded="false">
+                <i class="fa fa-users dashicon" aria-hidden="true"></i>Group Classes</a>
+                <ul id="submenu-1" class="collapse" aria-expanded="false" style="height: 0px;">
+                    <li><a href="#"> Host a Group Classes</a></li>
+                    <li><a href="#"> My Classes</a></li>
                 </ul>
             </li>
 
-
-            <!-- Reports -->
             <li>
-                <a href="javascript:;">
-                    <i class="toggle-accordion"></i>
-                    <!-- <i class="ti-support"></i> -->
-                    <i class="fa fa-line-chart"></i>
-                    <span>Reports</span>
-                </a>
-                <ul class="sub-menu">
-                <a href="javascript:;">
-                    <i class="toggle-accordion"></i>
-                    <!-- <i class="ti-support"></i> -->
-                    <i class="fa fa-line-chart"></i>
-                    <span>- Schedule</span>
-                </a>
-                <a href="javascript:;">
-                    <i class="toggle-accordion"></i>
-                    <!-- <i class="ti-support"></i> -->
-                    <i class="fa fa-line-chart"></i>
-                    <span>- Sessions Payment</span>
-                </a>
-               
+                <a href="#" data-toggle="collapse" data-target="#submenu-2" class="collapsed" aria-expanded="false">
+                <i class="fa fa-line-chart dashicon" aria-hidden="true"></i>Live Sessions</a>
+                <ul id="submenu-2" class="collapse" aria-expanded="false" style="height: 0px;">
+                    <li><a href="#"> Paid Sessions</a></li>
+                    <li><a href="#"> Unpaid Sessions</a></li>
                 </ul>
             </li>
-            <!-- /Reports -->
+
+            <li>
+                <a href="#">
+                    <span><i class="fa fa-sign-out dashicon" aria-hidden="true"></i>Logout</span>
+                </a>
+            </li>
+
         </ul>
-        <!-- class="active" -->
-      
-        <a href="javascript:;">
-            <i class="toggle-accordion"></i>
-            <!-- <i class="ti-settings"></i> -->
-            <i class="fa fa-table" aria-hidden="true"></i>
-            <span>Modules</span>
-        </a>
-        <!-- <a href="{{asset('user/trainer')}}"><i class="flaticon-loupe"></i> Search Trainers</a>
-        <a href="{{asset('user/trainer')}}"><i class="flaticon-trainer"></i> Featured Trainers</a>
-        <a href="#"><i class="flaticon-live"></i> Live Sessions</a>
-        <a href="#"><i class="flaticon-monitor"></i> Free Live Sessions</a>
-        <a href="#"><i class="flaticon-team"></i> Group Classes</a>
-        <a href="{{asset('user/nutrition')}}"><i class="flaticon-calculator"></i> Nutration Calculator</a>
-        <a href="#"><i class="flaticon-calendar"></i> My Schedule</a> -->
-        <a href="{{asset('user/logout')}}"><i class="flaticon-logout"></i> Logout</a>
 
     </div>
 
@@ -113,14 +50,20 @@
         @yield('dashboard')
     </div>
 </section>
+
+
 <footer>
     <div class="copyRightArea">
         <div class="container">
-            Copyrights © 2020. HomeFitLive All Rights Reserved. Designed By HatInco
+            <div class="row">
+                <div class="col-sm-12">
+                    <p>Copyrights © 2020. HomeFitLive All Rights Reserved. Designed By HatInco</p>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
+
 </body>
 @yield('app_jquery')
-
 </html>
